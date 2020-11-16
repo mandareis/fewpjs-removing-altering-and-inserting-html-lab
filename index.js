37510e849893066e75df8011519b8d9016a7a815
mandareis <amanda@depaula.xyz>
2020-11-16 14:30:50 -0800

    commit tree 1ccf1fa311d8d795eea64de2b947b4c4b7490251

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+let main2 = document.querySelector("main#main");
+main2.remove();
+
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+
+newHeader.innerHTML = "Amanda is the champion";
